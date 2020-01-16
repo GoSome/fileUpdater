c5930200d14800dfc7f562206831b15d46d90365
wbswjc <me@wbswjc.com>
2020-01-16 23:00:32 +0800

    Import ace editor

--- vue/src/components/UpdaterEdit.vue
+++ vue/src/components/UpdaterEdit.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="">
     <p>Editing updater {{ $route.query.name }}</p>
-    <b-textarea v-model="content" rows="20" class="mb-2"/>
+    <div class="my-3" v-if="initialized">
+      <editor v-model="content"
+              @init="editorInit"
+              :lang="lang"
+              class="rounded"
+              theme="dracula"
+              height="500"></editor>
+    </div>
     <b-button variant="primary" @click="save">Save</b-button>
     <b-button class="ml-2" @click="cancel">Cancel</b-button>
     <b-modal id="updated-modal"
@@ -15,19 +22,29 @@
 
 <script>
 export default {
+  components: {
+    editor: require('vue2-ace-editor'),
+  },
   data() {
     return {
+      initialized: false,
       content: '',
+      lang: 'json',
     }
   },
   created() {
     this.refresh()
   },
   methods: {
+    editorInit() {
+      require('brace/ext/language_tools')
+      require('brace/mode/json')
+      require('brace/theme/dracula')
+    },
     save() {
       this.$http.post('/api/content', {
         name: this.$route.query.name,
-        content: this.content
+        content: this.content,
       }).then(() => {
         this.refresh()
         this.$bvModal.show('updated-modal')
@@ -38,7 +55,8 @@ export default {
     },
     refresh() {
       this.$http.get('/api/content?name=' + this.$route.query.name).then(r => {
-        this.content = r.data
+        this.content = r.data.content
+        this.initialized = true
       }, r => {
         console.log(r)
         alert('Networking error')
