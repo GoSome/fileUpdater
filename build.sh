bdc86b1cdb5bd6581f7657dc7dd9ce06ece648eb
man <nothing@go.com>
2024-09-04 12:42:20 +0800

    add tag

--- build.sh
+++ build.sh
@@ -21,3 +21,9 @@ GOOS=linux GOARCH=arm64 go build -o ../bin/fileupdater-arm64-linux
 echo "clean statics"
 
 rm -rf build
+# Push binaries to GitHub release
+echo "Pushing binaries to GitHub release"
+VERSION=$(git describe --tags --abbrev=0)
+gh release create $VERSION bin/fileupdater-amd64-linux bin/fileupdater-arm64-linux --generate-notes
+
+echo "Release $VERSION created and binaries uploaded successfully"
